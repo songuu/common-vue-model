0592969739c0df59926cec3ba367d18c67b1d488
songyu <1101309860@qq.com>
2019-09-29 17:55:41 +0800

    增加文章

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 /* 
   1.在不同的环境配置前缀
   2.打包文件使用，大的打包文件通过cdn直接引入
-  3.在chainWebpack里面配置router和其他的别名设置，在configureWebpack里面设置打包和插件设置(通常为自定义插件)
+  3.在chainWebpack里面配置router和其他的别名设置以及配置自定义的loader或者修改默认loader的属性设置，在configureWebpack里面设置打包和插件设置(通常为自定义插件)
+  4.自定义的loader引入  
 */
 
 const path = require('path');
@@ -10,12 +11,34 @@ const resolve = dir => path.join(__dirname, dir);
 const BASE_URL = process.env.NODE_ENV === 'production' ? './' : '/';
 
 module.exports = {
-    publicPath: BASE_URL,
-    lintOnSave: process.env.NODE_ENV !== 'production',
-    chainWebpack: (config) => {
-        config.resolve.alias
-            .set('@', resolve('src'))
-            .set('_c', resolve('src/components'));
+        publicPath: BASE_URL,
+        lintOnSave: process.env.NODE_ENV !== 'production',
+        chainWebpack: (config) => {
+            config.resolve.alias
+                .set('@', resolve('src'))
+                .set('_c', resolve('src/components'));
+            config.module
+                .rule('vue')
+                .use('vue-loader')
+                .loader('vue-loader')
+                .tap(options => {
+                    return options
+                });
+            config.module
+                .rule('expose1')
+                .test(require.resolve('jquery'))
+                .use()
+                .loader('expose-loader')
+                .options("jQuery")
+                .end()
+            config.module
+                .rule('expose2')
+                .test(require.resolve('jquery'))
+                .use()
+                .loader('expose-loader')
+                .options("$")
+                .end()
+        }
     },
     configureWebpack: {
         externals: {
